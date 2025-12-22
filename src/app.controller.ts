dd8127bd7848cff1e450f7813238fa766b426716
furqanrupom <furqanrupom978@gmail.com>
2025-12-22 20:52:40 +0600

    feat : test post request

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,21 @@
-import { Controller, Get, Header } from '@nestjs/common';
+import { Body, Controller, Get,Param,Post,Req } from '@nestjs/common';
 import { AppService } from './app.service';
+import type {Request} from "express"
+import { UserDto } from './dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  @Header('Content-Type', 'text/html')
-  getHello(): string {
+  getHello(@Req() request:Request): string {
+    console.log(request.params)
     return this.appService.getHello();
+  }
+
+  @Get('/hello/:id')
+  getNice(@Req() request: Request, @Param() params:{id:string}): string {
+    return `Id Numb is ${params.id}`
   }
 
   @Get('/health')
@@ -19,5 +26,10 @@ export class AppController {
   @Get('/version')
   getVersion(): string {
     return '1.0.0';
+  }
+  @Post('/create-user')
+  createUser(@Body() userDto: UserDto):UserDto {
+     const data =  this.appService.createUser(userDto)
+     return data
   }
 }
