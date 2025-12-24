d2b0a4448cf244081b08a7c460a145e492bf1bbd
furqanrupom <furqanrupom978@gmail.com>
2025-12-25 02:30:31 +0600

    fix: type issues

--- src/common/filters/all-exceptions.filter.ts
+++ src/common/filters/all-exceptions.filter.ts
@@ -8,22 +8,7 @@ import {
     Logger,
 } from '@nestjs/common';
 import { Request, Response } from 'express';
-import { MongoError } from 'mongodb';
 import { Error as MongooseError } from 'mongoose';
-
-interface ValidationError {
-    errors: Record<string, { message: string }>;
-}
-
-interface CastError extends MongooseError.CastError {
-    path: string;
-    value: any;
-}
-
-interface DuplicateKeyError extends MongoError {
-    code: 11000;
-    keyValue: Record<string, any>;
-}
 
 @Catch()
 export class AllExceptionsFilter implements ExceptionFilter {
@@ -34,48 +19,43 @@ export class AllExceptionsFilter implements ExceptionFilter {
         const response = ctx.getResponse<Response>();
         const request = ctx.getRequest<Request>();
 
-        let status: number = HttpStatus.INTERNAL_SERVER_ERROR;
+        let status = HttpStatus.INTERNAL_SERVER_ERROR;
         let message: string | string[] = 'Internal server error';
-        let errorName: string = 'InternalServerError';
+        let errorName = 'InternalServerError';
 
         if (exception instanceof HttpException) {
             status = exception.getStatus();
             const res = exception.getResponse();
-            message =
-                typeof res === 'string'
-                    ? res
-                    : (res as any).message || exception.message;
+            message = typeof res === 'string' ? res : (res as object)['message'] || exception.message;
             errorName = exception.name;
         }
-        else if (
-            exception instanceof MongooseError.ValidationError ||
-            (exception as ValidationError)?.errors
-        ) {
+        else if (exception instanceof MongooseError.ValidationError) {
             status = HttpStatus.BAD_REQUEST;
-            const validationError = exception as MongooseError.ValidationError;
-            message = Object.values(validationError.errors).map((err) => err.message);
+            message = Object.values(exception.errors).map((err) => err.message);
             errorName = 'ValidationError';
         }
         else if (exception instanceof MongooseError.CastError) {
             status = HttpStatus.BAD_REQUEST;
-            const castError = exception as CastError;
-            message = `Invalid ${castError.path}: ${castError.value}`;
+            message = `Invalid ${exception.path}: ${exception.value}`;
             errorName = 'CastError';
         }
-        else if ((exception as DuplicateKeyError)?.code === 11000) {
+        else if (
+            exception &&
+            typeof exception === 'object' &&
+            'code' in exception &&
+            (exception as any).code === 11000 &&
+            'keyValue' in exception &&
+            (exception as any).keyValue
+        ) {
             status = HttpStatus.CONFLICT;
-            const dupError = exception as DuplicateKeyError;
-            const field = Object.keys(dupError.keyValue)[0];
-            const value = dupError.keyValue[field];
+            const keyValue = (exception as any).keyValue;
+            const field = Object.keys(keyValue)[0];
+            const value = keyValue[field];
             message = `${field.charAt(0).toUpperCase() + field.slice(1)} '${value}' already exists`;
             errorName = 'DuplicateKeyError';
         }
         else {
-            if ((exception as MongoError)?.code) {
-                this.logger.error(`MongoDB Error (code ${(exception as MongoError).code}):`, exception);
-            } else {
-                this.logger.error('Unexpected error:', exception);
-            }
+            this.logger.error('Unexpected error:', exception);
         }
 
         response.status(status).json({
