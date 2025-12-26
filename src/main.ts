15e1d1cd435ed516f6b553b34cc9ef62d9b763d2
furqanrupom <furqanrupom978@gmail.com>
2025-12-27 00:28:19 +0600

    chore(docs) fixed docs issue

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,16 @@ import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule,{cors:true});
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix('api/v1');
-  app.useGlobalFilters(new AllExceptionsFilter());
-
-
   const config = new DocumentBuilder()
     .setTitle('Do Quest API')
     .setDescription('API for managing quests and tasks')
     .setVersion('1.0.0')
     .build();
-    const documentFactory = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api/v1/docs', app, documentFactory);
+  const documentFactory = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/v1/docs', app, documentFactory);
+  app.useGlobalFilters(new AllExceptionsFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
 
