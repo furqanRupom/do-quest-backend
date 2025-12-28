ffd34e50558e938841de6baa41cb14feb0a92892
furqanrupom <furqanrupom978@gmail.com>
2025-12-29 03:42:51 +0600

    fix : import issue

--- src/auth/guards/jwt-auth-guard.ts
+++ src/auth/guards/jwt-auth-guard.ts
@@ -6,7 +6,7 @@ import {
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Reflector } from '@nestjs/core';
-import { UserRole } from '../enums';
+import { UserRole } from '../enums/role.enum';
 import { ROLES_KEY } from '../../common/decorators/roles.decorators';
 
 @Injectable()
