3b54e4ea3c8197be3dde4d1c0f0b5a3f243765a2
furqanrupom <furqanrupom978@gmail.com>
2026-01-04 23:11:35 +0600

    feat: added enums to tasks schema

---
+++ src/tasks/enums/tasks.enum.ts
@@ -0,0 +1,12 @@
+export enum TaskStatus {
+    pending = 'PENDING',
+    accepted = 'ACCEPTED',
+    rejected = 'REJECTED',
+    withdrawn = 'WITHDRAWN',
+}
+
+export enum PaymentStatus {
+    active = 'ACTIVE',
+    completed = 'COMPLETED',
+    cancelled = 'CANCELLED',
+}
--- src/tasks/schemas/tasks.schema.ts
+++ src/tasks/schemas/tasks.schema.ts
@@ -1,42 +1,50 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { Types } from "mongoose";
 import { User } from "../../users/schemas/users.schema";
+import { PaymentStatus, TaskStatus } from "../enums/tasks.enum";
 
-@Schema({timestamps:true})
+
+@Schema({ timestamps: true })
 export class Task {
     @Prop({ type: Types.ObjectId, ref: User.name, required: true, index: true })
     user: Types.ObjectId;
 
-    @Prop({required:true})
-    userId:string;
-    
-    @Prop({required:true})
-    title:string;
+    @Prop({ required: true })
+    userId: string;
 
+    @Prop({ required: true })
+    title: string;
 
-    @Prop({required:true})
-    description:string;
+    @Prop({ required: true })
+    description: string;
 
-    @Prop({required:true})
-    successRequirements:string;
+    @Prop({ required: true })
+    successRequirements: string;
 
-    @Prop({required:false})
-    attachments:string
+    @Prop({ required: false })
+    attachments: string
 
-    @Prop({required:true})
-    budget:number;
+    @Prop({ required: true })
+    budget: number;
 
-    @Prop({required:true})
-    deadline:string;
+    @Prop({ required: true })
+    deadline: string;
 
-    @Prop({required:false})
-    maxSubmissions:number;
+    @Prop({ required: false })
+    maxSubmissions: number;
 
-    @Prop({required:true})
+    @Prop({ enum: Object.values(TaskStatus) ,required: true, default: TaskStatus.pending,index:true })
+    status: TaskStatus
+
+    @Prop({ enum: Object.values(PaymentStatus), required: true, default: PaymentStatus.active })
+    paymentStatus: PaymentStatus
+
+    @Prop({ required: true })
     categories: string[];
 
-    @Prop({required:false})
+    @Prop({ required: false })
     tags: string[];
+
 }
 
 export const TaskSchema = SchemaFactory.createForClass(Task)
