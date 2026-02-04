f00a95fb7d25f418ff557c32f26f3f1b6a16257e
furqanrupom <furqanrupom978@gmail.com>
2026-02-04 22:43:03 +0600

    chore: fixed importing issues in admin module

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -2,11 +2,18 @@ import { Module } from '@nestjs/common';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 import { AdminRepository } from './admin.repository';
+import { UsersModule } from '../users/users.module';
+import { TasksModule } from '../tasks/tasks.module';
+import { SubmissionModule } from '../submission/submission.module';
+
 
 @Module({
-    imports: [AdminModule],
+    imports: [
+      UsersModule,
+      TasksModule,
+      SubmissionModule
+    ],
     controllers: [AdminController],
-    providers: [AdminService,AdminRepository],
-    exports: [],
+    providers: [AdminService, AdminRepository],
 })
-export class AdminModule {}
+export class AdminModule { }
