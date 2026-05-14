6cc4466c4aee803f496f696f6971e18b4688348f
furqanrupom <furqanrupom978@gmail.com>
2026-05-14 22:41:01 +0600

    chore(payment): added auth guards which is causing error

--- src/payment/payment.controller.ts
+++ src/payment/payment.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Param, Post, Req } from '@nestjs/common';
+import { Body, Controller, Param, Post, Req, UseGuards } from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { Roles } from '../common/decorators';
 import { UserRole } from '../auth/enums/role.enum';
@@ -6,16 +6,18 @@ import { ApiOkResponse } from '@nestjs/swagger';
 import { CreateNewTaskDto, CreateNewTaskResponseDto } from '../tasks/dto';
 import type { AuthRequest } from '../auth/types/auth-request.type';
 import { sendResponse } from '../common/utils';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth-guard';
 
 @Controller('payment')
 export class PaymentController {
 constructor(private readonly paymentService:PaymentService){}
 
- 
+    @UseGuards(JwtAuthGuard)
     @Roles(UserRole.User)
     @Post("tasks")
     @ApiOkResponse({ type: CreateNewTaskResponseDto })
     async createNewTask(@Body() createTaskDto: CreateNewTaskDto, @Req() req: AuthRequest): Promise<CreateNewTaskResponseDto> {
+        console.log(req.user.sub)
         const result = await this.paymentService.createTaskWithPayment(createTaskDto, req.user.sub)
         return sendResponse({
             success: true,
@@ -24,7 +26,7 @@ constructor(private readonly paymentService:PaymentService){}
         })
     }
 
-    
+    @UseGuards(JwtAuthGuard)
     @Roles(UserRole.User)
     @Post("tasks/:id/cancel")
     async cancelTask(@Param('id')id:string, @Req() req: AuthRequest) {
